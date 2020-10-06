028e5833ffe8bcd37c1a9705bc5112b163d85903
Aleksandr Sova <sovasasha1@gmail.com>
2020-10-06 22:56:27 +0300

    Lab4 (Activity Lifecycle)

--- app/src/main/java/com/chnulabs/books/BookActivity.java
+++ app/src/main/java/com/chnulabs/books/BookActivity.java
@@ -1,13 +1,16 @@
 package com.chnulabs.books;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
 public class BookActivity extends AppCompatActivity {
+    private float textSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,12 @@ public class BookActivity extends AppCompatActivity {
         }
         TextView textView = findViewById(R.id.text);
         textView.setText(txtBooks.toString());
+        textSize = textView.getTextSize();
+        if (savedInstanceState != null){
+            textSize = savedInstanceState.getFloat("textSize");
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+
+        }
     }
 
     public void onBtnSendClick(View view){
@@ -33,5 +42,16 @@ public class BookActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_TITLE, "Список літератури");
         intent.putExtra(Intent.EXTRA_TEXT, textView.getText());
         startActivity(intent);
+    }
+    public void onBtnPlusClick(View view){
+        textSize *= 1.1f;
+        TextView textView = findViewById(R.id.text);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putFloat("textSize", textSize);
     }
 }
--- app/src/main/java/com/chnulabs/books/MainActivity.java
+++ app/src/main/java/com/chnulabs/books/MainActivity.java
@@ -1,21 +1,76 @@
 package com.chnulabs.books;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+import javax.net.ssl.HandshakeCompletedEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private Boolean isRunning = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null){
+            seconds = savedInstanceState.getInt("timer");
+        }
+        runTimer();
     }
 
     public void onBtnClick(View view){
         Intent intent = new Intent(this, BookActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("timer", seconds);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isRunning = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isRunning = false;
+    }
+
+    private void runTimer(){
+        final TextView timeView = findViewById(R.id.timer);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 }
